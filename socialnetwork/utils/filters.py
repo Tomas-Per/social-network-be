55fe298d03721889383773cf1495c6774d6ba2f3
Tomas <tomasperveneckas9@gmail.com>
2023-05-07 17:14:24 +0300

    add trending filter

--- socialnetwork/utils/filters.py
+++ socialnetwork/utils/filters.py
@@ -1,4 +1,7 @@
-from django.db.models import QuerySet
+from datetime import timedelta
+
+from django.db.models import Count, Q, QuerySet
+from django.utils import timezone
 from django_filters import rest_framework as filters
 
 from community.models import Community
@@ -25,12 +28,12 @@ class CommunityFilter(filters.FilterSet):
 
 class PostFilter(filters.FilterSet):
     full_text_search = filters.CharFilter(method="full_text_search_filter", label="search")
+    trending = filters.BooleanFilter(method="trending_filter", label="trending")
 
     order = filters.OrderingFilter(
         fields=(
             ("created_at", "created_at"),
             ("updated_at", "updated_at"),
-            ("vote_count", "vote_count"),
         )
     )
 
@@ -46,6 +49,17 @@ class PostFilter(filters.FilterSet):
     def full_text_search_filter(self, queryset: QuerySet, _, value: str) -> QuerySet:
         results = get_elastic_results("posts", ["title", "content"], value)
         return queryset.filter(id__in=results)
+
+    def trending_filter(self, queryset: QuerySet, _, value: bool) -> QuerySet:
+        if value:
+            queryset = queryset.filter(created_at__gte=timezone.now() - timedelta(days=7))
+
+        queryset.annotate(
+            vote_count=Count("post_votes", filter=Q(post_votes__vote_type=1))
+            - Count("post_votes", filter=Q(post_votes__vote_type=-1))
+        ).order_by("-vote_count")
+
+        return queryset
 
 
 class CommentFilter(filters.FilterSet):
